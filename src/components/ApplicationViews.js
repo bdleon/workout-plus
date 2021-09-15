456f406e16819f59b91a8c15f5d8404bfd0117dc
Brandon De Leon <brandondeleon.615@gmail.com>
2021-09-15 10:18:43 -0500

    added login and register page.Also added logout link in navbar

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,9 @@
 import React from "react"
 import { Route } from "react-router-dom"
+
+
+
+
 import { PostForm } from "./feed/PostForm"
 import { PostList } from "./feed/PostList"
 
@@ -8,12 +12,14 @@ import { PostList } from "./feed/PostList"
 export const ApplicationViews = () => {
     return (
         <>
-        <Route exact path="/posts">
+        <Route exact path="/">
             <PostList />
         </Route>
         <Route path="/posts/create">
             <PostForm />
         </Route>
+        
+        
         </>
     )
 }
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,19 +1,34 @@
 import React from "react"
 import { Link } from "react-router-dom"
+
+
+
 import "./NavBar.css"
 
 export const NavBar = () => {
+
+
     return (
         <ul className="navbar">
             <li className="navbar__item active">
-                <Link className="navbar__link" to="/posts">Logo</Link>
+                <Link className="navbar__link" to="/">Logo</Link>
             </li>
             <li className="navbar__item active">
                 <Link className="navbar__link" to="/category">category</Link>
             </li>
+            
             <li className="navbar__item active">
-                <Link className="navbar__link" to="/">Logout</Link>
+                <Link className="navbar__link" to="#"
+                    onClick={
+                        () => {
+                            localStorage.removeItem("workout_token")
+                        }
+                    }>
+                    Logout
+                </Link>
             </li>
+
         </ul>
+
     )
 }
